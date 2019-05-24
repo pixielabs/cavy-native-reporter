b6ec60344f3c694915fa79f051432078621dff57
Abigail McPhillips <abigail.mcphillips@gmail.com>
2019-05-24 15:34:41 +0100

    Switch to connectedAndroidTest rather than test

--- sampleApp/android/app/src/androidTest/java/com/sampleapp/BridgeTest.java
+++ sampleApp/android/app/src/androidTest/java/com/sampleapp/BridgeTest.java
@@ -4,16 +4,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import com.cavynativereporter.RNCavyNativeReporterModule;
 
-/**
- * To work on unit tests, switch the Test Artifact in the Build Variants view.
- */
 public class BridgeTest {
   @Test
   public void testBridge() throws Exception {
     RNCavyNativeReporterModule.onFinish(new Runnable() {
       @Override
       public void run() {
-        assertEquals(true, true);
+        assertEquals(true, false);
       }
     });
   }
